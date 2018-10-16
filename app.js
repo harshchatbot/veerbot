ad2671d70bc056fb6a17fc5d4536f2b2ec28837c
harshchatbot <40229369+harshchatbot@users.noreply.github.com>
2018-10-17 00:27:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@
 
 const
   bodyParser = require('body-parser'),
-  config = require('config'),
+  config = require('./config.json'),
   crypto = require('crypto'),
   express = require('express'),
   https = require('https'),
