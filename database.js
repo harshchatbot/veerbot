9cbc41d6dea9b75febcae268a95197c0d0c3b174
harshchatbot <40229369+harshchatbot@users.noreply.github.com>
2018-10-27 16:42:11 +0530

    Update database.js

--- database.js
+++ database.js
@@ -25,10 +25,6 @@ client.query('SELECT NOW()', (err, res) => {
 */
 
 
-var express = require('express');
-var pg = require("pg");
-var app = express();
-var http = require('http');
 
 /*
 
@@ -42,30 +38,23 @@ server.listen(1212); // listening on port 1212
 */
 
  
-var connectionString = "postgres://qokezostymfzhl:4b68816ad2118d0a9945afe846ff1a6704425ac891976b5a3162d4c8969fe149@ec2-54-83-3-101.compute-1.amazonaws.com:5432/dabdf4hps4beb";
  
-app.get('/', function (req, res, next) {
-    pg.connect(connectionString,function(err,client,done) {
-       if(err){
-           console.log("not able to get connection "+ err);
-           res.status(400).send(err);
-       } 
-       client.query('SELECT * FROM chatdata where userid = 3', [1],function(err,result) {
-       	if (err) throw err;
-    	console.log(result);
-       	
-           done(); // closing the connection;
-           if(err){
-               console.log(err);
-               res.status(400).send(err);
-           }
-           res.status(200).send(result.rows);
-           
-           
-       });
-    });
-});
  
-app.listen(5432, function () {
-    console.log('Server is running.. on Port 5432');
+var express = require('express');
+const pg = require('pg');
+var app = express();
+var http = require('http');
+
+
+
+ 
+const pool = new pg.Pool({
+connectionString : 'postgres://qokezostymfzhl:4b68816ad2118d0a9945afe846ff1a6704425ac891976b5a3162d4c8969fe149@ec2-54-83-3-101.compute-1.amazonaws.com:5432/dabdf4hps4beb',
+ssl: true,
 });
+
+pool.query("INSERT INTO chatdata VALUES(6,'mahesh','logtest6')", (err, res) => {
+console.log(err, res);
+pool.end();
+});
+
